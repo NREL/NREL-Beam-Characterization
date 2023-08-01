e617df51215f931edb4c005799a4f5bf845f402d
Jack deBloois <jack.debloois@nrel.gov>
2023-08-01 11:12:43 -0600

    added open cv sample file

--- BCS_Single.py
+++ BCS_Single.py
@@ -29,101 +29,37 @@ import pypylon
 #imageFiles = sorted(glob.glob("Y:/5700/SolarElectric/PROJECTS/38488_HelioCon_Zhu/BeamCharacterizationSystems/OTF.07.21.22/otf79/*.jpg"),key=len)
 #imageFiles = sorted(glob.glob("Y:/5700/SolarElectric/PROJECTS/38488_HelioCon_Zhu/BeamCharacterizationSystems/OTF.07.08.22/*.jpg"),key=len)
 
-# This sample demonstrates how to use action commands on a GigE camera to
-# trigger images. Since this feature requires configuration of several camera
-# features, this configuration is encapsuled in a dedicated configuration
-# event handler called ActionTriggerConfiguration, whose usage is also
-# demonstrated.
-
 from pypylon import pylon
 
-tl_factory = pylon.TlFactory.GetInstance()
+# conecting to the first available camera
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
 
-tlf = pylon.TlFactory.GetInstance()
-tl = tlf.CreateTl('BaslerGigE')
-cam_info = tl.CreateDeviceInfo()
-cam_info.SetIpAddress('192.168.3.3')
-cam = pylon.InstantCamera(tlf.CreateDevice(cam_info))
+# Grabing Continusely (video) with minimal delay
+camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+converter = pylon.ImageFormatConverter()
 
-cam.Open()
-cam.ActionDeviceKey.SetValue(0)
-cam.ActionGroupKey.SetValue(0)
-cam.ActionGroupMask.SetValue(0)
-cam.TriggerSource.SetValue('Line1')
-cam.TriggerMode.SetValue('Off')
-cam.AcquisitionMode.SetValue('SingleFrame')
-print("ActionDeviceKey", hex(cam.ActionDeviceKey.GetValue()))
-print("ActionGroupKey", hex(cam.ActionGroupKey.GetValue()))
-print("ActionGroupMask", hex(cam.ActionGroupMask.GetValue()))
-print("TriggerSource", cam.TriggerSource.GetValue())
-print("TriggerMode", cam.TriggerMode.GetValue())
-print("AcquisitionMode", cam.AcquisitionMode.GetValue())
-print('\n')
-cam.Close()
+# converting to opencv bgr format
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
-# Values needed for action commands. See documentation for the meaning of these
-# values: https://docs.baslerweb.com/index.htm#t=en%2Faction_commands.htm
-# For this simple sample we just make up some values.
+while camera.IsGrabbing():
+    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
 
-action_key = 0x4711
-group_key = 0x112233
-group_mask = pylon.AllGroupMask
+    if grabResult.GrabSucceeded():
+        # Access the image data
+        image = converter.Convert(grabResult)
+        img = image.GetArray()
+        cv2.namedWindow('title', cv2.WINDOW_NORMAL)
+        cv2.imshow('title', img)
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
+    grabResult.Release()
+    
+# Releasing the resource    
+camera.StopGrabbing()
 
-# Initiate automatic configuration by registering ActionTriggerConfiguration.
-cam.RegisterConfiguration(
-    pylon.ActionTriggerConfiguration(action_key, group_key, group_mask),
-    pylon.RegistrationMode_Append,
-    pylon.Cleanup_Delete
-    )
-cam.Open()
-
-# Create a suitable ActionCommand object. For that a GigETransportLayer object
-# is needed.
-gige_tl = tl_factory.CreateTl('BaslerGigE')
-
-# Using default value of "255.255.255.255" for fourth
-# parameter 'broadcastAddress'.
-act_cmd = gige_tl.ActionCommand(action_key, group_key, group_mask)
-
-# possible results for issuing an action command
-act_cmd_status_strings = {
-    pylon.GigEActionCommandStatus_Ok:
-        'The device acknowledged the command',
-    pylon.GigEActionCommandStatus_NoRefTime:
-        'The device is not synchronized to a master clock',
-    pylon.GigEActionCommandStatus_Overflow:
-        'The action commands queue is full',
-    pylon.GigEActionCommandStatus_ActionLate:
-        'The requested action time was at a point in time that is in the past',
-    }
-
-
-cam.StartGrabbing()
-
-for counter in range(1, 9):
-
-    # Issue action command
-    if counter & 1:
-        # use no-wait variant on odd counter
-        print('issuing no-wait action command')
-        ok = act_cmd.IssueNoWait()
-        assert ok
-    else:
-        # use waiting variant on even counter
-        print('issuing action command with waiting for response')
-        timeout_ms = 1000
-        expected_results = 1
-        ok, results = act_cmd.IssueWait(timeout_ms, expected_results)
-        print('action command results')
-        assert ok
-        for addr, status in results:
-            print(addr, act_cmd_status_strings[status])
-
-    with cam.RetrieveResult(1000, pylon.TimeoutHandling_ThrowException):
-        print("received frame %d\n" % counter)
-
-cam.StopGrabbing()
-cam.Close()
+cv2.destroyAllWindows()
 
 
 for i in range(len(imageFiles)):
