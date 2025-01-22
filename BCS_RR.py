8b7b10a4693be64b3b3ee3e17581f24962656dd0
Zheng, Grant <Grant.Zheng@nrel.gov>
2025-01-22 11:08:25 -0700

    Updated doc

--- BCS_RR.py
+++ BCS_RR.py
@@ -12,8 +12,8 @@ def find_centroid_wrapper(img_path: str, visualization:bool = False) -> np.ndarr
     corners = BCS_functions.find_corner_candidates(img)
     valid_corners = BCS_functions.valid_intersections(corners, img.shape)
     rectified_img = BCS_functions.rectify_and_crop(img_centroid, valid_corners)
-    # rectified_img_filtered = BCS_functions.low_pass_filter(rectified_img, keep_ratio=0.02)
-    rectified_img_filtered = cv2.medianBlur(rectified_img, 35)
+    rectified_img_filtered = BCS_functions.low_pass_filter(rectified_img, keep_ratio=0.02)
+    # rectified_img_filtered = cv2.medianBlur(rectified_img, 35)
     rectified_img_gamma_filtered = BCS_functions.gamma_correction(rectified_img_filtered, 7)
 
     centroid_location = BCS_functions.find_centroid(rectified_img_gamma_filtered)
@@ -21,19 +21,23 @@ def find_centroid_wrapper(img_path: str, visualization:bool = False) -> np.ndarr
     if visualization:
         import matplotlib.pyplot as plt
         fig, axs = plt.subplots(1, 2)
-        axs[0].imshow(rectified_img, cmap='gray')
-        axs[0].set_title("Rectified image")
-        axs[0].axis('off')
-        axs[1].imshow(rectified_img_gamma_filtered, cmap='gray')
-        axs[1].scatter(centroid_location[0], centroid_location[1], c='r', s=20)
-        axs[1].set_title("Gamma corrected image")
+        axs[1].imshow(rectified_img, cmap='gray')
+        axs[1].set_title("Original image")
         axs[1].axis('off')
+        axs[0].imshow(rectified_img_gamma_filtered, cmap='gray')
+        axs[0].scatter(centroid_location[0], centroid_location[1], c='r', s=20)
+        axs[0].set_title("Gamma corrected image")
+        axs[0].axis('off')
         plt.show()
 
     return centroid_location
 
 
 def tracking_error_finder(heliostat_position: np.ndarray, target_position: np.ndarray, center_measurements: np.ndarray):
+    """
+    Relies on the dot product to find 
+    """
+    
     ideal_vector = target_position - heliostat_position
     measured_vector = ideal_vector.copy()
     measured_vector[0] += center_measurements[0]    # x measruement off
@@ -81,11 +85,12 @@ if __name__ == "__main__":
                 axs[1].axis('off')
 
                 plt.show()
+        # Sandia data does not need borner finding, the excel sheet does not have targer width either
         elif row["Source"] == "Sandia":
             _, img = BCS_functions.load_image(img_path)
             img = cv2.resize(img, (1000, 1000))
-            # img_low_pass = BCS_functions.low_pass_filter(img, keep_ratio=0.1)
-            img_low_pass = cv2.medianBlur(img, 35)
+            img_low_pass = BCS_functions.low_pass_filter(img, keep_ratio=0.1)
+            # img_low_pass = cv2.medianBlur(img, 35)
             img_gamma = BCS_functions.gamma_correction(img_low_pass, 2)
             centroid_location = BCS_functions.find_centroid(img_gamma)
 
@@ -108,6 +113,8 @@ if __name__ == "__main__":
                 plt.show()
 
         elif row["Source"] == "CENER":
+            # CENER data uses latitude and longtiude to find the centroid
+            # Here I manually converted the data into distances and hard coded 
             row["HeliostatX"] = 0
             row["HeliostatY"] = 0
             row["HeliostatZ"] = 0
